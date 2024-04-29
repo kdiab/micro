ff3109da9b3cffd86f61ec5e8586e9f64586762c
Diab <khalid@diab.cc>
2024-04-29 17:16:06 -0400

    buttons

--- App.js
+++ App.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
-const App = () => {
+const Price = props => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    return (
+        <View style={styles.priceContainer}>
+            <Button
+                onPress={() => {
+                  setIsClicked(!isClicked);
+                }}
+                title={isClicked ? props.rate : props.currency}
+             />
+        </View>
+    )
+}
+
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>micro! 🎉</Text>
+    <View style={styles.container}>
+        <Price currency="$" rate="36.6Bs"/>
+        <Price currency="E" rate="76.6Bs"/>
+        <Price currency="Y" rate="26.6Bs"/>
+      <StatusBar style="auto" />
     </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#FCFBF4',
+    justifyContent: 'space-around',
+    alignItems: 'top',
+  },
+  priceContainer: {
+    flex: 1,
+    alignItems: 'center'
+  }
+});
